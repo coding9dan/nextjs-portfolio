a19913b7ceab8d27bf3ffcf812ed6a39655adc20
Michael Lim <dongseok.lim@gmail.com>
2023-12-31 20:41:59 -0600

    add new game link

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -31,7 +31,7 @@ const Game = () => {
                 </p>
               </div>
               <div className="h-12 w-12 bg-white rounded-full flex justify-center items-center cursor-pointer">
-                <Link href="http://jsgame.us.to/" target="_blank">
+                <Link href="https://jscarrots.netlify.app/" target="_blank">
                   <Arrow className="w-6 h-6" />
                 </Link>
               </div>
